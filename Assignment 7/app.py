5ea172c9f0d847a7ff7864ee97567715f216ae9e
Prateek Agrawal <138208959+Prateek0305@users.noreply.github.com>
2025-07-20 17:42:22 +0530

    Update app.py

--- Assignment 7/app.py
+++ Assignment 7/app.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+model = joblib.load("iris_model.pkl")
+
+
+st.title("🌸 Iris Species Prediction App")
+st.write("Enter flower measurements to predict the species.")
+
+
+sepal_length = st.sidebar.slider("Sepal Length", 4.0, 8.0, 5.0)
+sepal_width = st.sidebar.slider("Sepal Width", 2.0, 4.5, 3.0)
+petal_length = st.sidebar.slider("Petal Length", 1.0, 7.0, 4.0)
+petal_width = st.sidebar.slider("Petal Width", 0.1, 2.5, 1.0)
+
+
+input_df = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]],
+                        columns=["sepal_length", "sepal_width", "petal_length", "petal_width"])
+
+if st.button("Predict"):
+    prediction = model.predict(input_df)[0]
+    st.success(f"🌼 Predicted Species: **{prediction}**")
+
+if st.checkbox("Show sample data"):
+    df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+    st.dataframe(df.head())
+
+if st.checkbox("Show visualization"):
+    df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
+    fig, ax = plt.subplots()
+    sns.scatterplot(x='sepal_length', y='sepal_width', hue='species', data=df, ax=ax)
+    st.pyplot(fig)
 
